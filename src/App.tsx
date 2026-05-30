a7dfd5130977001ceee429931b1c34fabae7941a
zairgb <jairmjia.gb@gmail.com>
2026-05-29 20:51:14 -0600

    configuracion del autocompletado de tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,13 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="min-h-screen bg-zinc-950 text-white flex flex-col items-center justify-center">
-      <h1 className="text-4xl text-center text-white font-bold pt-10">
-        Welcome to Vite + React!
+    <div className="min-h-screen flex items-center justify-center bg-gray-950">
+      <h1 className="text-5xl font-bold text-white bg">
+        React + TypeScript + Tailwind + pnpm 🚀
       </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
